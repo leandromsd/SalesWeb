cf4a313415d6afd3ce47509b8fb5d296724ce014
Leandro Caixeta Ferreira <caixeta.leandro@gmail.com>
2020-09-07 09:24:01 -0300

    Simple Search

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+
+        private readonly SalesRecordsService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordsService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var salesRecords = await _salesRecordsService.FindByDateAsync(minDate, maxDate);
+            return View(salesRecords);
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+
+    }
+}
